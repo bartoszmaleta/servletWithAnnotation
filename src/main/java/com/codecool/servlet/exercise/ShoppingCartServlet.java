29ab914f0a8edccc910b6852b0d530c032535edc
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-08-15 13:52:20 +0200

    Implement doGet()

--- src/main/java/com/codecool/servlet/exercise/ShoppingCartServlet.java
+++ src/main/java/com/codecool/servlet/exercise/ShoppingCartServlet.java
@@ -1,5 +1,53 @@
 package com.codecool.servlet.exercise;
 
-public class ShoppingCartServlet {
-    
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet (name = "userCardsHandler", urlPatterns = {"/cards"}, loadOnStartup = 1)
+public class ShoppingCartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getMethod().equals("GET")) {
+            resp.setStatus(200);
+            resp.getWriter().println(generateWeb());
+        }
+
+    }
+
+    private String generateWeb() {
+        List<List<String>> items = new ArrayList<>();
+        for (Item item : Card.items) {
+            List<String> row = Arrays.asList(
+                    String.valueOf(item.id)
+                    , item.name
+                    , String.valueOf(item.price));
+            items.add(row);
+        }
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/userCards.twig");
+        JtwigModel model = JtwigModel.newModel();
+        model.with("items", items);
+        model.with("price", sumItemsCost());
+        return template.render(model);
+
+    }
+
+    private int sumItemsCost() {
+        int sum = 0;
+        for (Item item : Card.items) {
+            sum += item.price;
+        }
+        return sum;
+    }
+
 }
